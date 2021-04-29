dff8afb13abc02cc8c45bd1e0d735f3cd06177b5
Mighty-encoder <73258767+Mighty-encoder@users.noreply.github.com>
2021-04-28 23:47:11 -0300

    Trabajo Terminado.

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,36 @@
 module Library where
 import PdePreludat
-
 doble :: Number -> Number
-doble numero = numero + numero
+doble numero = 2*numero
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas `div` fortaleza objetivo
+
+poder :: Horas -> Number
+poder = (* 15)
+
+fortaleza :: Objetivo -> Number
+fortaleza = (2*) . length
+
+type Golpe = Objetivo -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe 
+normalesConsecutivos = presionGolpe 240
+
+objetivoDificil :: Objetivo -> Bool
+objetivoDificil = (<100).gomuGomu
+
+objetivoAccesible :: Objetivo -> Bool
+objetivoAccesible = estaEntre.normalesConsecutivos.objetivoFocalizado
+
+estaEntre :: Number -> Bool
+estaEntre number = number > 200 && number < 400 
+
+objetivoFocalizado :: Objetivo -> Objetivo 
+objetivoFocalizado = take 7 
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,35 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+  describe "poder" $ do
+    it "Es el poder del golpe 15*Golpe" $ do
+      poder 10 `shouldBe` 150
+  
+  describe "fortaleza" $ do
+    it "Es la fortaleza del golpe (2*) . length" $ do
+      fortaleza "hola" `shouldBe` 8
+  
+  describe "presionGolpe" $ do
+    it "presionGolpe 200 horas sobre bolsa de entrenamiento" $ do
+     round (presionGolpe 200 "bolsa de entrenamiento") `shouldBe` 68
+  
+  describe "gomuGomu" $ do
+    it "gomugomu 180 sobre bolsa de entrenamiento" $ do
+     round (presionGolpe 180 "bolsa de entrenamiento") `shouldBe` 61
+  describe "normalesConsecutivos" $ do
+    it "normalesConsecutivos 240 sobre bolsa de entrenamiento" $ do
+     round (presionGolpe 240 "bolsa de entrenamiento") `shouldBe` 81  
+  
+  describe "objetivoDificil" $ do
+    it "objetivo es Dificil si gomuGomu ejerce presion < 100 sobre bolsa de entrenamiento" $ do
+      objetivoDificil "bolsa de entrenamiento" `shouldBe` True
+  describe "objetivoDificil" $ do
+    it "objetivo no es Dificil si gomuGomu ejerce presion > 100 sobre puf" $ do
+      objetivoDificil "puf" `shouldBe` False
 
+  describe "objetivoAccesible" $ do
+    it "objetivo es Accesible si la presion de normalesConsecutivos esta entre 200 y 400 estando focalizado el objetivo" $ do
+      objetivoAccesible "bolsa de entrenamiento" `shouldBe` True
+  describe "objetivoAccesible" $ do
+    it "objetivo no es Accesible si la presion de normalesConsecutivos esta por fuera de 200 y 400 estando focalizado el objetivo" $ do
+      objetivoAccesible "puf" `shouldBe` False            
