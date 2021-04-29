a1fc5402b4bc18e18ccf639b8b5fd719c83a3d78
Mighty-encoder <73258767+Mighty-encoder@users.noreply.github.com>
2021-04-29 16:03:02 -0300

    Nombre de los Test's Arreglados

--- src/Spec.hs
+++ src/Spec.hs
@@ -6,34 +6,34 @@ import Test.Hspec
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "poder" $ do
-    it "Es el poder del golpe 15*Golpe" $ do
+    it "Es el poder de un golpe" $ do
       poder 10 `shouldBe` 150
   
   describe "fortaleza" $ do
-    it "Es la fortaleza del golpe (2*) . length" $ do
+    it "Es la fortaleza de un golpe (2*) . length" $ do
       fortaleza "hola" `shouldBe` 8
   
   describe "presionGolpe" $ do
-    it "presionGolpe 200 horas sobre bolsa de entrenamiento" $ do
+    it "presion de un Golpe" $ do
      round (presionGolpe 200 "bolsa de entrenamiento") `shouldBe` 68
   
   describe "gomuGomu" $ do
-    it "gomugomu 180 sobre bolsa de entrenamiento" $ do
+    it "presion de gomugomu" $ do
      round (presionGolpe 180 "bolsa de entrenamiento") `shouldBe` 61
   describe "normalesConsecutivos" $ do
-    it "normalesConsecutivos 240 sobre bolsa de entrenamiento" $ do
+    it "presion de normalesConsecutivos" $ do
      round (presionGolpe 240 "bolsa de entrenamiento") `shouldBe` 81  
   
   describe "objetivoDificil" $ do
-    it "objetivo es Dificil si gomuGomu ejerce presion < 100 sobre bolsa de entrenamiento" $ do
+    it "objetivo es Dificil" $ do
       objetivoDificil "bolsa de entrenamiento" `shouldBe` True
   describe "objetivoDificil" $ do
-    it "objetivo no es Dificil si gomuGomu ejerce presion > 100 sobre puf" $ do
+    it "objetivo no es Dificil" $ do
       objetivoDificil "puf" `shouldBe` False
 
   describe "objetivoAccesible" $ do
-    it "objetivo es Accesible si la presion de normalesConsecutivos esta entre 200 y 400 estando focalizado el objetivo" $ do
+    it "objetivo es Accesible" $ do
       objetivoAccesible "bolsa de entrenamiento" `shouldBe` True
   describe "objetivoAccesible" $ do
-    it "objetivo no es Accesible si la presion de normalesConsecutivos esta por fuera de 200 y 400 estando focalizado el objetivo" $ do
+    it "objetivo no es Accesible" $ do
       objetivoAccesible "puf" `shouldBe` False            
